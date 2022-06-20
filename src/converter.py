7d77b8d5f1feffff6a1ad7753849e3eb410ac1a7
Vania Natalie <71621406+VaniaNatalie@users.noreply.github.com>
2022-06-20 12:51:55 +0700

    Delete converter.py

--- src/converter.py
+++
@@ -1,7 +0,0 @@
-class Convert:
-    def __init__(self, s):
-        self.s = s
-        self.output = '#include <iostream>\nusing namespace std;\n'
-
-    def convert(self):
-        return self.output
